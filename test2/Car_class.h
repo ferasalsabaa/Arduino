10de8178f4626d88ea5e1efab558e530a6c58af0
Feras al sabaa <feras.alsabaa@uni-weimar.de>
2019-11-30 17:32:26 +0100

    add return back to the car method

--- test2/Car_class.h
+++ test2/Car_class.h
@@ -1,39 +1,59 @@
-class Car_class{
+class Car_class {
 
-public:
+  public:
 
-int now=0;    // where is the car
+    int now = 0;  // where is the car
 
 
 
 #define ctsPin 19
-int sensorValue  = 0;
+    int sensorValue  = 0;
 
-void testMethod(int& sensorValue, int& now, CRGB led[], int NUM_LEDS){
-      if (sensorValue > 900)   //modify this threshold and relaunchVel to adjust sensitivity
-    { int count=0;
-      if(now == 20 || now == 21 || now == 22 || now == 23 || now == 24 || now == 25){
-            led[now].setRGB(0,0,220);
-                   FastLED.show();
-                   delay(1000);
-      now = now + 1 ;
+    void moveAlong(int& sensorValue, int& now, CRGB led[], int NUM_LEDS, int& sensorValueB) {
+      if (sensorValueB < 300)   //modify this threshold and relaunchVel to adjust sensitivity
+      {
+        if (now == 20 || now == 21 || now == 22 || now == 23 || now == 24 || now == 25) {
+          led[now].setRGB(0, 0, 220);
+          FastLED.show();
+          delay(100);
+          now = now + 1 ;
         } else {
-      led[now].setRGB(220,62,220);
-      FastLED.show();
-      now = now + 1 ;
+          if (now == 0) {
+            led[now].setRGB(220, 62, 220);
+            FastLED.show();
+            delay(100);
+          } else {
+            led[now].setRGB(220, 62, 220);
+            FastLED.show();
+            delay(100);
+            now = now - 1 ;
+          }
         }
-      if(now == NUM_LEDS){
-        now = 0;
-      for(int i=0;i<NUM_LEDS;i++){
-      fill_rainbow(led, i, 0, 5);
-      FastLED.show();
-       delay(10);
-  }
-        } 
-        
+
+      } else if (sensorValue < 300) {
+        if (now == 20 || now == 21 || now == 22 || now == 23 || now == 24 || now == 25) {
+          led[now].setRGB(0, 0, 220);
+          FastLED.show();
+          delay(1000);
+          now = now + 1 ;
+        } else {
+          led[now].setRGB(220, 62, 220);
+          FastLED.show();
+          delay(100);
+          now = now + 1 ;
+        }
+        if (now == NUM_LEDS) {
+          now = 0;
+          for (int i = 0; i < NUM_LEDS; i++) {
+            fill_rainbow(led, i, 0, 5);
+            FastLED.show();
+            delay(10);
+          }
+        }
+
+      }
+
     }
-  
-}  
-  
- 
+
+
 };
--- test2/test2.ino
+++ test2/test2.ino
@@ -11,10 +11,23 @@ const int ledLength = 8;     // later long of car
 #define MAX_BRIGHTNESS  100
 const int fps = 100;
 
-int now=0;    // where is the car
+int now = 0;  // where is the car
 
 #define ctsPin 19  // place the sensor
 int sensorValue  = 0;  // touch value
+int sensorValueB  = 0;
+
+//VARIABLEN ERZEUGEN
+int sensorPinA = 15;    //hier ist die nummer des analogen pins gespeichert an dem unser sensor angeschlossen ist, ggf. anpassen! (z.b. 16, 17, 20, oder 21)
+int sensorPinB = 16;
+int sensorPinC = 17;
+int sensorPinD = 18;
+int sensorWertA = 0;    //in dieser variable wird unser sensorwert, d.h. der Rückgabewert der Funktion analogRead(); oder touchRead(); gespeichert.
+int sensorWertB = 0;
+int sensorWertC = 0;
+int sensorWertD = 0;
+int sensorValueTest =0;
+
 
 //create chrono objects for timed actions:
 Chrono cNextFrame(Chrono::MICROS);
@@ -27,42 +40,58 @@ Car_class car1; // first car
 
 void setup() {
 
-  FastLED.addLeds<NEOPIXEL,LED_PIN>(led, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(led, NUM_LEDS);
 
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(led, NUM_LEDS);
   FastLED.setCorrection(TypicalLEDStrip); //LEDS.setCorrection(Candle); or (Tungsten40W)
   FastLED.setBrightness(MAX_BRIGHTNESS);
 
-      Serial.begin(9600);
-       pinMode(sensorValue, OUTPUT);
-       pinMode(ctsPin, INPUT);
-} 
+  Serial.begin(115200); //serielle datenverbindung zum computer herstellen damit wir mit Serial.print(); oder Serial.println(); daten senden können
+  pinMode(sensorValue, OUTPUT);
+  pinMode(ctsPin, INPUT);
+
+  pinMode(sensorPinA, INPUT_PULLUP);  //declare pin as input AND enable internal 33kohm pullup resistor (that is: a 33k resistor between the analog input pin and 3.3 volts)
+  pinMode(sensorPinB, INPUT_PULLUP);
+  pinMode(sensorPinC, INPUT_PULLUP);
+  pinMode(sensorPinD, INPUT_PULLUP);
+}
 
 void loop()
-    {
-   sensorValue = touchRead(ctsPin);
+{
+ // sensorValue = filter(analogRead(sensorPinA), 0.5, sensorWertA);    
+ //  sensorWertA = filter(analogRead(sensorPinA), 0.25, sensorWertA);  
+//  sensorValueTest = map(sensorValue, 0, 1023, 0, 100);
+ // sensorValue = sensorWertA
+ sensorValue = analogRead(sensorPinA);
+ sensorValueB = analogRead(sensorPinB);
   Serial.println(sensorValue);               //send debug data to the Arduino Serial Monitor
 
-  
+
   //INTERACTIVITY: work with sensor data every X ms
-  if (cCheckInput.hasPassed(1)){
+  if (cCheckInput.hasPassed(1)) {
     cCheckInput.restart();
-    car1.testMethod(sensorValue, now, led, NUM_LEDS);
+    car1.moveAlong(sensorValue, now, led, NUM_LEDS, sensorValueB);
   }
   //DRAW FRAME
   if (cNextFrame.hasPassed((1000 * 1000) / fps) ) { //milliseconds chrono -> triggers on every frame...
-  
+
     cNextFrame.restart();
     FastLED.clear();
-led[20].setRGB(220,0,0);
-led[21].setRGB(220,0,0);
-led[22].setRGB(220,0,0);
-led[23].setRGB(220,0,0);
-led[24].setRGB(220,0,0);
-led[25].setRGB(220,0,0);
-led[now].setRGB(220,62,220);
+    led[20].setRGB(220, 0, 0);
+    led[21].setRGB(220, 0, 0);
+    led[22].setRGB(220, 0, 0);
+    led[23].setRGB(220, 0, 0);
+    led[24].setRGB(220, 0, 0);
+    led[25].setRGB(220, 0, 0);
+    led[now].setRGB(220, 62, 220);
 
     FastLED.show();
   }
-  
-      } // end loop
+
+} // end loop
+
+float filter(float rawValue, float weight, float lastValue)
+{
+  float result = weight * rawValue + (1.0 - weight) * lastValue;
+  return result;
+}
