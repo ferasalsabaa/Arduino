606b66053526f720d1bb233d41db672497506c07
Feras al sabaa <feras.alsabaa@uni-weimar.de>
2019-12-03 00:37:45 +0100

    add filter and map methods

--- test2/Car_class.h
+++ test2/Car_class.h
@@ -10,7 +10,7 @@ class Car_class {
     int sensorValue  = 0;
 
     void moveAlong(int& sensorValue, int& now, CRGB led[], int NUM_LEDS, int& sensorValueB) {
-      if (sensorValueB < 300)   //modify this threshold and relaunchVel to adjust sensitivity
+      if (sensorValueB < 8)   //modify this threshold and relaunchVel to adjust sensitivity
       {
         if (now == 20 || now == 21 || now == 22 || now == 23 || now == 24 || now == 25) {
           led[now].setRGB(0, 0, 220);
@@ -23,21 +23,21 @@ class Car_class {
             FastLED.show();
             delay(100);
           } else {
-            led[now].setRGB(220, 62, 220);
+          //  led[now].setRGB(220, 62, 220);
             FastLED.show();
             delay(100);
             now = now - 1 ;
           }
         }
 
-      } else if (sensorValue < 300) {
+      } else if (sensorValue < 8) {
         if (now == 20 || now == 21 || now == 22 || now == 23 || now == 24 || now == 25) {
           led[now].setRGB(0, 0, 220);
           FastLED.show();
           delay(1000);
           now = now + 1 ;
         } else {
-          led[now].setRGB(220, 62, 220);
+        //  led[now].setRGB(220, 62, 220);
           FastLED.show();
           delay(100);
           now = now + 1 ;
