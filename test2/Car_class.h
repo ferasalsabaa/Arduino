30c1aa53a4bb2b6f1ded410cd14de3c1bf89acc6
Feras al sabaa <feras.alsabaa@uni-weimar.de>
2019-12-03 19:40:26 +0100

    delete the old method

--- test2/Car_class.h
+++ test2/Car_class.h
@@ -1,52 +1,6 @@
 class Car_class {
 
   public:
-
-
-    /* void moveAlong(int& sensorValue, int& now, CRGB led[], int NUM_LEDS, int& sensorValueB) {
-       if (sensorValueB < 8)   //modify this threshold and relaunchVel to adjust sensitivity
-       {
-         if (now == 20 || now == 21 || now == 22 || now == 23 || now == 24 || now == 25) {
-           led[now].setRGB(0, 0, 220);
-           FastLED.show();
-           delay(100);
-           now = now + 1 ;
-         } else {
-           if (now == 0) {
-             led[now].setRGB(220, 62, 220);
-             FastLED.show();
-             delay(100);
-           } else {
-             FastLED.show();
-             delay(100);
-             now = now - 1 ;
-           }
-         }
-
-       } else if (sensorValue < 9) {
-         if (now == 20 || now == 21 || now == 22 || now == 23 || now == 24 || now == 25) {
-           led[now].setRGB(0, 0, 220);
-           FastLED.show();
-           delay(1000);
-           now = now + 1 ;
-         } else {
-           FastLED.show();
-           delay(100);
-           now = now + 1 ;
-         }
-         if (now == NUM_LEDS) {
-           now = 0;
-           for (int i = 0; i < NUM_LEDS; i++) {
-             fill_rainbow(led, i, 0, 5);
-             FastLED.show();
-             delay(10);
-           }
-         }
-
-       }
-
-      }*/
-
     void move_now_speed(int& sensorValue, int& now, CRGB led[], int NUM_LEDS, int& sensorValueB) {
       if (sensorValueB < 7 && sensorValue < 7) {
         if (now == 20 || now == 21 || now == 22 || now == 23 || now == 24 || now == 25) {
