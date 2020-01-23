2b9962212b317cf0e5f33b08ee4c8a38b27d60e6
Feras al sabaa <feras.alsabaa@uni-weimar.de>
2020-01-23 11:10:57 +0100

    change the fire places

--- NEW_Prototype/NEW_main_Prototype/NEW_main_Prototype.ino
+++ NEW_Prototype/NEW_main_Prototype/NEW_main_Prototype.ino
@@ -47,8 +47,8 @@ CRGB led [NUM_LEDS];
 // ---------------------------------player 1------------------------------------------
 int OnePlayerPosition = 6;
 
-float OneFireFirstPosition = 7;
-float OneFireSecondPosition = 8;
+float OneFireFirstPosition = 5;
+float OneFireSecondPosition = 6;
 
 float OnePlayerFireFirst = 0.0;
 float OnePlayerFireSecond = 0.0;
@@ -63,8 +63,8 @@ Player player1(OnePlayerPosition, OneFireFirstPosition, OneFireSecondPosition, O
 // ---------------------------------player 2------------------------------------------
 int TwoPlayerPosition = 137;
 
-float TwoFireFirstPosition = 136;
-float TwoFireSecondPosition = 135;
+float TwoFireFirstPosition = 138;
+float TwoFireSecondPosition = 137;
 
 float TwoPlayerFireFirst = 0.0;
 float TwoPlayerFireSecond = 0.0;
@@ -396,58 +396,4 @@ void fadeToBlack(int ledNo, byte fadeValue) {
   // FastLED
   led[ledNo].fadeToBlackBy( fadeValue );
 #endif
-}
-// -------------------------------Fire -----------------------------------------------
-void Fire(int Cooling, int Sparking, int SpeedDelay) {
-  static byte heat[NUM_LEDS];
-  int cooldown;
-
-  // Step 1.  Cool down every cell a little
-  for ( int i = 0; i < NUM_LEDS; i++) {
-    cooldown = random(0, ((Cooling * 10) / NUM_LEDS) + 2);
-
-    if (cooldown > heat[i]) {
-      heat[i] = 0;
-    } else {
-      heat[i] = heat[i] - cooldown;
-    }
-  }
-
-  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-  for ( int k = NUM_LEDS - 1; k >= 2; k--) {
-    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
-  }
-
-  // Step 3.  Randomly ignite new 'sparks' near the bottom
-  if ( random(255) < Sparking ) {
-    int y = random(7);
-    heat[y] = heat[y] + random(160, 255);
-    //heat[y] = random(160,255);
-  }
-
-  // Step 4.  Convert heat to LED colors
-  for ( int j = 0; j < NUM_LEDS; j++) {
-    setPixelHeatColor(j, heat[j] );
-  }
-
-  showStrip();
-  delay(SpeedDelay);
-}
-
-void setPixelHeatColor (int Pixel, byte temperature) {
-  // Scale 'heat' down from 0-255 to 0-191
-  byte t192 = round((temperature / 255.0) * 191);
-
-  // calculate ramp up from
-  byte heatramp = t192 & 0x3F; // 0..63
-  heatramp <<= 2; // scale up to 0..252
-
-  // figure out which third of the spectrum we're in:
-  if ( t192 > 0x80) {                    // hottest
-    setPixel(Pixel, 255, 255, heatramp);
-  } else if ( t192 > 0x40 ) {            // middle
-    setPixel(Pixel, 255, heatramp, 0);
-  } else {                               // coolest
-    setPixel(Pixel, heatramp, 0, 0);
-  }
 }
